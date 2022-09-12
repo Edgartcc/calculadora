9039cda81210f99c54e4f0f6ae207ffbbcf24a94
Edgar Tipanguano <edgar.tipanguano@outlook.com>
2022-09-11 21:24:03 -0500

    Primer commit

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-//suma, resta, multiplicación, división
-functioncalculadora(num1,operador, num2)
-  {
-let resultado;
-switch (operador) {  // Utilice la estructura de control de Switch
-case "+": resultado = num1 + num2;
-break;
-case "-": resultado = num1 - num2;
-break;
-case "*": resultado = num1 * num2;
-break;
-case "/": resultado = num1 / num2;
-break;
-default:  resultado = "Operador inexistente" // Cuando se ingrese cualquier otro dato que no sea uno de los operadores aritmeticos, aparecera este mensaje.
-break;
+//Encontrar el número mayor de una lista de números en un array
+  
+let numeros = [5, 10 ,15, 60, 8]
+let numeroMaximo = 0
+let tamano = numeros.length //array.lenght me entrega la cantidad de posiciones de un arrya que creamos anteriormente
+
+for(let i=0; i<tamano; i++) {
+    if(numeroMaximo < numeros[i]) {
+        numeroMaximo = numeros[i]
     }
-    console.log(resultado);
-  }
+}
 
-num1 = parseInt(prompt("Ingrese un numero ")); // Utilice la funcion "parseInt" para devolver un numero entero del string; y el metodo "prompt" para mostrar un dialogo con mensaje opcional, que solicite al usuario que introduzca un texto.
-operador = prompt("Elija operador: +, -, *, / ");
-num2 = parseInt(prompt("Ingrese otro numero "));
-console.log(num1, num2);
+console.log(numeroMaximo)
 
-
-calculadora(num1, operador, num2); // Llame a la funciòn
+let maximo = Math.max(5, 10, 15, 60, 8)
+console.log(maximo)
